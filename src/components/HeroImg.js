9a0f5c687d5086853d8d1f3ef667e6ea00fa2fbb
chrisrdaniels <chrisdaniels765@gmail.com>
2022-06-16 17:16:28 -0600

    added css animations to h and p tags on pages

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -11,7 +11,8 @@ const HeroImg = () => {
 				<img className='intro-img' src={IntroImage} alt='IntroImage' />
 			</div>
 			<div className='content'>
-				<p>HI, I'M A FREELANCE</p>
+				<h1>Hello.</h1>
+				<p>MY NAME IS CHRIS, I'M A FREELANCE</p>
 				<h1>React Developer.</h1>
 				<Link to='/projects' className='btn'>
 					Projects
