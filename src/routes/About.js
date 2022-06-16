dcd481223c3e7115856ecb83e427e8e516be289f
chrisrdaniels <chrisdaniels765@gmail.com>
2022-06-16 17:37:03 -0600

    added text/card css animations

--- src/routes/About.js
+++ src/routes/About.js
@@ -8,7 +8,7 @@ const About = () => {
 	return (
 		<div>
 			<Navbar />
-			<HeroImg2 heading='ABOUT.' text="I'm Chris, a Freelance Developer." />
+			<HeroImg2 heading='ABOUT.' text="I'm Chris, a Freelance Front-End Developer Located In Colorado. I Specialize in creating Responsive Websites/Apps With React." />
 			<AboutContent />
 			<Footer />
 		</div>
