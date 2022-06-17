f52459558f2b9fac610c44b5463b9a825efa71d6
chrisrdaniels <chrisdaniels765@gmail.com>
2022-06-16 20:19:46 -0600

    added a skills route

---
+++ src/routes/Skills.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroImg2 from '../components/HeroImg2';
+import Footer from '../components/Footer';
+
+const Skills = () => {
+	return (
+		<div>
+			<Navbar />
+			<HeroImg2
+				heading='SKILLS.'
+				text='The Various Languages/Frameworks/Tech Stacks That I Have Worked With In The Past.'
+			/>
+			<Footer />
+		</div>
+	);
+};
+
+export default Skills;
