7b784f6a96b02706437cfa4027f559aa04e4adb9
chrisrdaniels <chrisdaniels765@gmail.com>
2022-06-16 19:58:20 -0600

    added pricing route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './routes/Home';
 import About from './routes/About';
 import Contact from './routes/Contact';
 import Projects from './routes/Projects';
+import Pricing from './routes/Pricing';
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 				<Route path='projects' element={<Projects />} />
 				<Route path='about' element={<About />} />
 				<Route path='contact' element={<Contact />} />
+				<Route path='pricing' element={<Pricing />} />
 			</Routes>
 		</>
 	);
